3b70f79661e764db728bddf46ee96054ed6c33e9
IbrahimIbrahim2001 <ii8538098@gmail.com>
2025-11-16 21:04:27 +0300

    feat: modify workspace, fix auth middleware session fix: - fix session in auth middleware, nit it  is returning the session successfully - workspace projects added asChild to remove the warning modify: -workspace page to prefetch the workspace query new:  -create a workspace avatar file with a random color for the fallback -create workspace list and workspace components to view the user workspaces

--- app/(root)/workspace/page.tsx
+++ app/(root)/workspace/page.tsx
@@ -1,44 +1,13 @@
-"use client";
-import { Logo } from "@/components/logo"
-import { Button } from "@/components/ui/button"
-import {
-    Empty,
-    EmptyContent,
-    EmptyDescription,
-    EmptyHeader,
-    EmptyMedia,
-    EmptyTitle,
-} from "@/components/ui/empty"
-import { CreateWorkSpace } from "../components/create-workspace"
-import { authClient } from "@/lib/auth-client";
-export default function WorksSpacePage() {
-    const { data: workspaces } = authClient.useListOrganizations();
-    if (!workspaces || workspaces.length === 0) {
-        return (
-            <div className='p-4'>
-                <Empty>
-                    <EmptyHeader>
-                        <EmptyMedia variant="icon">
-                            <Logo width={32} height={32} />
-                        </EmptyMedia>
-                        <EmptyTitle className="text-secondary font-semibold">No Workspaces Yet</EmptyTitle>
-                        <EmptyDescription>
-                            You haven&apos;t created any workspaces yet. Get started by creating
-                            your first workspace.
-                        </EmptyDescription>
-                    </EmptyHeader>
-                    <EmptyContent>
-                        <CreateWorkSpace trigger={<Button >Create Workspace</Button>} />
-                    </EmptyContent>
-                </Empty>
-            </div>
-        )
-    }
+
+import { getQueryClient, HydrateClient } from "@/lib/query/hydration"
+import { orpc } from "@/lib/orpc"
+import { WorkspaceList } from "./components/workspace-list";
+export default async function WorksSpacePage() {
+    const queryClient = getQueryClient();
+    await queryClient.prefetchQuery(orpc.workspace.list.queryOptions());
     return (
-        <>
-            {workspaces.map((workspace) => (
-                <p key={workspace.id}>{workspace.name}</p>
-            ))}
-        </>
+        <HydrateClient client={queryClient}>
+            <WorkspaceList />
+        </HydrateClient>
     )
 }
--- app/middleware/auth-middleware.ts
+++ app/middleware/auth-middleware.ts
@@ -1,12 +1,14 @@
 import { base } from "./base"
 import { User } from "better-auth"
-import { authClient } from "@/lib/auth-client"
 import { redirect } from "next/navigation"
-
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 export const requiredAuthMiddleware = base
     .$context<{ session?: { user?: User } }>()
     .middleware(async ({ context, next }) => {
-        const session = context.session ?? ((await authClient.getSession()).data);
+        const session = context.session ?? (await auth.api.getSession({
+            headers: await headers()
+        }))
         if (!session?.user) {
             redirect('../login')
         }
