c420ea40dff8e297fdca52e133678899925b223e
IbrahimIbrahim2001 <ii8538098@gmail.com>
2025-12-02 21:39:44 +0300

    feat: role-and-status-badge modify: - nav-bar.tsx:    now get the project status and member role - schema.ts:    create new field for project status with 'in progress',   'finished' states - create-project.ts:     add status to the newProject object for optimistic  updates - project.ts:    added a badge for project status -get-role-badge-color.ts:   --added new  functions to get the project and member role badge background and border and text color

--- app/(root)/utils/get-role-badge-color.ts
+++ app/(root)/utils/get-role-badge-color.ts
@@ -1,3 +1,5 @@
+import { ProjectSchema } from "@/db/schema";
+
 export type Role = "owner" | "admin" | "member";
 export function getBadgeColor(role: Role): string {
     switch (role) {
@@ -5,5 +7,50 @@ export function getBadgeColor(role: Role): string {
         case "admin": return "bg-secondary";
         case "member": return "bg-accent";
         default: return "bg-primary";
+    }
+}
+
+export function getBadgeBorderColor(role: Role): string {
+    switch (role) {
+        case "owner": return "border-primary";
+        case "admin": return "border-secondary ";
+        case "member": return "border-accent ";
+        default: return "border-primary";
+    }
+}
+
+export function getBadgeTextColor(role: Role): string {
+    switch (role) {
+        case "owner": return "text-primary";
+        case "admin": return "text-secondary";
+        case "member": return "text-accent";
+        default: return "text-primary";
+    }
+}
+
+
+
+export function getProjectStatusBadgeColor(status: ProjectSchema["status"] | undefined): string {
+    switch (status) {
+        case "in progress": return "bg-secondary";
+        case "finished": return "bg-destructive";
+        default: return "bg-secondary";
+    }
+}
+
+
+export function getProjectStatusBadgeBorderColor(status: ProjectSchema["status"] | undefined): string {
+    switch (status) {
+        case "in progress": return "border-secondary";
+        case "finished": return "border-destructive";
+        default: return "border-secondary";
+    }
+}
+
+export function getProjectStatusBadgeTextColor(status: ProjectSchema["status"] | undefined): string {
+    switch (status) {
+        case "in progress": return "text-secondary";
+        case "finished": return "text-destructive";
+        default: return "text-secondary";
     }
 }
